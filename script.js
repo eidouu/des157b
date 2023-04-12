16c34000985146819e038c50ae0bd86907be21e7
andre_lam <adelam@ucdavis.edu>
2023-04-12 12:27:46 -0700

    completed ver 1

--- script.js
+++ script.js
@@ -6,13 +6,15 @@
     const button = document.querySelector('button');
     const body = document.querySelector('body');
     const banner = document.querySelector('#banner');
-    const sections = document.querySelectorAll('section')
+    const image = document.querySelector('#banner img');
+    const sections = document.querySelectorAll('section');
     let mode = 'dark';
 
     button.addEventListener('click', function() {
         if (mode === 'dark') {
             body.className = 'switch';
             banner.className = 'switch';
+            image.src = "images/catday.svg"; 
             button.className = 'switch';
             for (const section of sections) {
                 section.className = 'switch';
@@ -21,6 +23,7 @@
         } else {
             body.removeAttribute('class');
             banner.removeAttribute('class');
+            image.src = "images/catnight.svg"; 
             button.removeAttribute('class');
             for (const section of sections) {
                 section.removeAttribute('class');
